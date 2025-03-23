a9ea6b78f570f12958b54333dcb2eb09cd116ea7
evelyn-zhan <evelynzanpx@gmail.com>
2025-03-23 20:52:06 +0700

    fix layout of articles page

--- lib/screens/articles.dart
+++ lib/screens/articles.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:health_mobile_app/components/article_page_card.dart';
+import 'package:health_mobile_app/components/article_card.dart';
 
 class Articles extends StatefulWidget {
   const Articles({super.key});
@@ -33,7 +33,7 @@ class _ArticlesState extends State<Articles> {
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFA),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(15, 75, 15, 40),
+        padding: EdgeInsets.fromLTRB(15, 70, 15, 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -43,38 +43,43 @@ class _ArticlesState extends State<Articles> {
               icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black, size: 21)
             ),
             SizedBox(height: 15),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Don't miss the latest articles!", style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w700)),
-                  Text('Learn more, gain knowledge', style: GoogleFonts.poppins(color: Color(0xFF898989), fontSize: 15, fontWeight: FontWeight.w600))
-                ]
-              )
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      ...articleList.map((article) {
-                        return ArticlePageCard(
-                          title: article["title"],
-                          imageUrl: article["imageUrl"],
-                          description: article["description"],
-                          dateCreated: article["dateCreated"],
-                          detail: article["detail"]
-                        );
-                      })
-                    ]
-                  )
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Don't miss the latest articles!", style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w700)),
+                          Text('Learn more, gain knowledge', style: GoogleFonts.poppins(color: Color(0xFF898989), fontSize: 15, fontWeight: FontWeight.w600)),
+                        ]
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Column(
+                        children: [
+                          ...articleList.map((article) {
+                            return ArticleCard(
+                              title: article['title'],
+                              imageUrl: article['imageUrl'],
+                              description: article['description'],
+                              dateCreated: article['dateCreated'],
+                              detail: article['detail']
+                            );
+                          })
+                        ]
+                      ),
+                    )
+                  ]
                 ),
               ),
-            )
+            ),
           ]
         ),
       )
