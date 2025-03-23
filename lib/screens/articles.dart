a00b7f1e1cfc15b4348dba8a94655ba3040a8fcf
evelyn-zhan <evelynzanpx@gmail.com>
2025-03-23 16:07:10 +0700

    add: articles page

--- lib/screens/articles.dart
+++ lib/screens/articles.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Articles extends StatelessWidget {
+import 'package:google_fonts/google_fonts.dart';
+
+class Articles extends StatefulWidget {
   const Articles({super.key});
 
   @override
+  State<Articles> createState() => _ArticlesState();
+}
+
+class _ArticlesState extends State<Articles> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
