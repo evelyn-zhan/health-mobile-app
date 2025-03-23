e6643e641970880a4a9fb6dac2767514396b23d5
evelyn-zhan <evelynzanpx@gmail.com>
2025-03-23 16:03:56 +0700

    fix: home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
-import 'package:health_mobile_app/components/featured_article_card.dart';
+
 import 'package:health_mobile_app/screens/articles.dart';
+import 'package:health_mobile_app/components/featured_article_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,7 +11,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF9F9F9),
+      backgroundColor: Color(0xFFFAFAFA),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Padding(
@@ -84,7 +86,7 @@ class Home extends StatelessWidget {
                             ]
                           ),
                           Spacer(),
-                          Image.asset('assets/shoe-icon.png', width: 30, height: 30)
+                          Image.asset('assets/images/shoe-icon.png', width: 30, height: 30)
                         ]
                       )
                     ),
@@ -121,7 +123,7 @@ class Home extends StatelessWidget {
                             ]
                           ),
                           Spacer(),
-                          Image.asset('assets/water-icon.png', width: 28, height: 28)
+                          Image.asset('assets/images/water-icon.png', width: 28, height: 28)
                         ]
                       )
                     ),
@@ -147,7 +149,7 @@ class Home extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image.asset('assets/3d-target.png', width: 45, height: 45),
+                    Image.asset('assets/images/3d-target.png', width: 45, height: 45),
                     SizedBox(width: 30),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -174,7 +176,7 @@ class Home extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image.asset('assets/mineral-water.png', width: 45, height: 45),
+                    Image.asset('assets/images/mineral-water.png', width: 45, height: 45),
                     SizedBox(width: 30),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -198,7 +200,8 @@ class Home extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(context, MaterialPageRoute(builder: (context) => Articles()));
                       },
-                      child: Text('See More Articles', style: GoogleFonts.poppins(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w500, decoration: TextDecoration.underline))
+                      style: TextButton.styleFrom(overlayColor: Colors.transparent),
+                      child: Text('See More Articles', style: GoogleFonts.poppins(color: Colors.black, fontSize: 13, fontWeight: FontWeight.w500, decoration: TextDecoration.underline))
                     )
                   ]
                 )
@@ -210,19 +213,19 @@ class Home extends StatelessWidget {
                 child: Row(
                   children: [
                     FeaturedArticleCard(
-                      imageUrl: 'assets/article-1.png',
+                      imageUrl: 'assets/images/article-1.png',
                       title: 'Why Sleep is Your Superpower for Better Health',
                       summary: 'Sleeping 100 hours will make you healthy'
                     ),
                     SizedBox(width: 10),
                     FeaturedArticleCard(
-                      imageUrl: 'assets/article-3.jpg',
+                      imageUrl: 'assets/images/article-3.jpg',
                       title: 'Hydration Hacks: Drink Smarter, Stay Healthier',
                       summary: 'Simple ways to track water intake and avoid dehydration'
                     ),
                     SizedBox(width: 10),
                     FeaturedArticleCard(
-                      imageUrl: 'assets/article-4.jpg',
+                      imageUrl: 'assets/images/article-4.jpg',
                       title: 'Superfoods You Should Eat Every Week',
                       summary: 'Nutrient-packed foods to improve overall well-being'
                     )
