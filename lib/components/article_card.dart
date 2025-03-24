4b3fbe562f97d3296394b5f6fe78186cbaaa38ed
evelyn-zhan <evelynzanpx@gmail.com>
2025-03-24 09:46:59 +0700

    fix code indentation

--- lib/components/article_card.dart
+++ lib/components/article_card.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ArticleCard extends StatelessWidget {
-  const ArticleCard({super.key, required this.title, required this.imageUrl, required this.description, required this.dateCreated, required this.detail});
+  const ArticleCard({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+    required this.dateCreated,
+    required this.detail
+  });
 
   final String title, imageUrl, description, dateCreated;
   final List detail;
