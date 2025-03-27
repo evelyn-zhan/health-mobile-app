b6caec1c337714e1bbcd86eafcbf6146daf8d3bf
evelyn-zhan <evelynzanpx@gmail.com>
2025-03-27 10:45:39 +0700

    add safe area to article details page

--- lib/screens/article_detail.dart
+++ lib/screens/article_detail.dart
@@ -27,143 +27,145 @@ class _ArticleDetailState extends State<ArticleDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFA),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.fromLTRB(10, 70, 10, 40),
-              color: Color(0xFF1E1E1E),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextButton.icon(
-                    onPressed: () => Navigator.pop(context),
-                    style: TextButton.styleFrom(overlayColor: Colors.transparent),
-                    icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 20),
-                    label: Text('Back', style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500, decoration: TextDecoration.underline, decorationColor: Colors.white))
-                  ),
-                  SizedBox(height: 15),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.fromLTRB(10, 30, 10, 40),
+                color: Color(0xFF1E1E1E),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () => Navigator.pop(context),
+                      style: TextButton.styleFrom(overlayColor: Colors.transparent),
+                      icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 20),
+                      label: Text('Back', style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500, decoration: TextDecoration.underline, decorationColor: Colors.white))
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(widget.title, maxLines: 2, style: GoogleFonts.poppins(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700)),
+                          SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.person_rounded, color: Colors.white, size: 20),
+                                  SizedBox(width: 5),
+                                  Text(widget.author, style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500))
+                                ]
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.calendar_month_rounded, color: Colors.white, size: 20),
+                                  SizedBox(width: 5),
+                                  Text(widget.dateCreated, style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500))
+                                ]
+                              )
+                            ]
+                          ),
+                          SizedBox(height: 25),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(widget.imageUrl, width: double.infinity, height: 160, fit: BoxFit.cover),
+                          )
+                        ]
+                      )
+                    )
+                  ]
+                )
+              ),
+              ...widget.detail.map((paragraph) {
+                return
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
+                  child: Text(paragraph, textAlign: TextAlign.justify, style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400))
+                );
+              }),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 30, 20, 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Comments', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w600)),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(widget.title, maxLines: 2, style: GoogleFonts.poppins(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700)),
-                        SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Icon(Icons.person_rounded, color: Colors.white, size: 20),
-                                SizedBox(width: 5),
-                                Text(widget.author, style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500))
-                              ]
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Icon(Icons.calendar_month_rounded, color: Colors.white, size: 20),
-                                SizedBox(width: 5),
-                                Text(widget.dateCreated, style: GoogleFonts.poppins(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500))
-                              ]
+                        Expanded(
+                          child: SizedBox(
+                            height: 40,
+                            child: TextField(
+                              controller: commentController,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.message_outlined, color: Color(0xFF1E1E1E), size: 20),
+                                label: Text('Type your comment here...', style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400)),
+                                floatingLabelStyle: GoogleFonts.poppins(color: Color(0xFF1E1E1E), fontSize: 14, fontWeight: FontWeight.w400),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Color(0xFF1E1E1E), width: 1),
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Color(0xFF1E1E1E), width: 1),
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                contentPadding: EdgeInsets.symmetric(vertical: 5)
+                              ),
+                              style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400)
                             )
-                          ]
+                          )
                         ),
-                        SizedBox(height: 25),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.asset(widget.imageUrl, width: double.infinity, height: 160, fit: BoxFit.cover),
+                        SizedBox(width: 5),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (commentController.text.isNotEmpty) {
+                                comments.add(commentController.text);
+                              }
+                              commentController.clear();
+                            });
+                          },
+                          style: IconButton.styleFrom(
+                            backgroundColor: Color(0xFF1E1E1E),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
+                          ),
+                          icon: Icon(Icons.send_rounded, color: Colors.white, size: 21)
                         )
                       ]
-                    )
-                  )
-                ]
-              )
-            ),
-            ...widget.detail.map((paragraph) {
-              return
-              Container(
-                padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
-                child: Text(paragraph, textAlign: TextAlign.justify, style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400))
-              );
-            }),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 30, 20, 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Comments', style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w600)),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: SizedBox(
-                          height: 40,
-                          child: TextField(
-                            controller: commentController,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.message_outlined, color: Color(0xFF1E1E1E), size: 20),
-                              label: Text('Type your comment here...', style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400)),
-                              floatingLabelStyle: GoogleFonts.poppins(color: Color(0xFF1E1E1E), fontSize: 14, fontWeight: FontWeight.w400),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xFF1E1E1E), width: 1),
-                                borderRadius: BorderRadius.circular(5)
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xFF1E1E1E), width: 1),
-                                borderRadius: BorderRadius.circular(5)
-                              ),
-                              contentPadding: EdgeInsets.symmetric(vertical: 5)
-                            ),
-                            style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400)
+                    ),
+                    ...comments.map((comment) {
+                      return
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Color(0xFFD9D9D9),
+                              width: 1.5
+                            )
                           )
-                        )
-                      ),
-                      SizedBox(width: 5),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (commentController.text.isNotEmpty) {
-                              comments.add(commentController.text);
-                            }
-                            commentController.clear();
-                          });
-                        },
-                        style: IconButton.styleFrom(
-                          backgroundColor: Color(0xFF1E1E1E),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
                         ),
-                        icon: Icon(Icons.send_rounded, color: Colors.white, size: 21)
-                      )
-                    ]
-                  ),
-                  ...comments.map((comment) {
-                    return
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color(0xFFD9D9D9),
-                            width: 1.5
-                          )
-                        )
-                      ),
-                      child: Text(comment, style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400))
-                    );
-                  })
-                ]
+                        child: Text(comment, style: GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400))
+                      );
+                    })
+                  ]
+                )
               )
-            )
-          ]
-        )
+            ]
+          )
+        ),
       )
     );
   }
