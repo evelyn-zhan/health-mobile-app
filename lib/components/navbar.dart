495a944de1772582a1fe3e0d7a29aa8130721ea4
evelyn-zhan <evelynzanpx@gmail.com>
2025-04-18 19:40:31 +0700

    Update design of bottom navbar

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:health_mobile_app/screens/home.dart';
 import 'package:health_mobile_app/screens/articles.dart';
+import 'package:health_mobile_app/screens/todo.dart';
 
 class Navbar extends StatelessWidget {
-  const Navbar({super.key, required this.page});
+  Navbar({super.key, required this.page});
 
   final String page;
+  List<Map<String, dynamic>> NavList = [
+    {
+      "widget": Home(),
+      "icon": Icons.home_rounded,
+      "text": "Home",
+    },
+    {
+      "widget": Articles(),
+      "icon": Icons.article_rounded,
+      "text": "Articles",
+    },
+    {
+      "widget": ToDo(),
+      "icon": Icons.task_outlined,
+      "text": "Tasks",
+    },
+    {
+      "widget": (),
+      "icon": Icons.person_2_rounded,
+      "text": "Profile",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,37 +47,64 @@ class Navbar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-            },
-            child: Column(
-              children: [
-                Icon(Icons.home_rounded, color: page == 'home' ? Colors.white : Color(0xFFC8C8C8), size: 30),
-                SizedBox(height: 3),
-                Text('Home', style: GoogleFonts.poppins(color: page == 'home' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
-              ]
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Articles()));
-            },
-            child: Column(
-              children: [
-                Icon(Icons.article_rounded, color: page == 'articles' ? Colors.white : Color(0xFFC8C8C8), size: 30),
-                SizedBox(height: 3),
-                Text('Articles', style: GoogleFonts.poppins(color: page == 'articles' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
-              ]
-            ),
-          ),
-          Column(
-            children: [
-              Icon(Icons.person_2_rounded, color: page == 'profile' ? Colors.white : Color(0xFFC8C8C8), size: 30),
-              SizedBox(height: 3),
-              Text('Profile', style: GoogleFonts.poppins(color: page == 'profile' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
-            ]
-          )
+          ...NavList.map((nav) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => nav["widget"]));
+              },
+              child: Column(
+                children: [
+                  Icon(nav["icon"], color: page == nav["text"] ? Colors.white : Color(0xFFC8C8C8), size: 30),
+                  SizedBox(height: 3),
+                  Text(nav["text"], style: GoogleFonts.poppins(color: page == nav["text"] ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
+                ]
+              ),
+            );
+          })
+
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+          //   },
+          //   child: Column(
+          //     children: [
+          //       Icon(Icons.home_rounded, color: page == 'home' ? Colors.white : Color(0xFFC8C8C8), size: 30),
+          //       SizedBox(height: 3),
+          //       Text('Home', style: GoogleFonts.poppins(color: page == 'home' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
+          //     ]
+          //   ),
+          // ),
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.push(context, MaterialPageRoute(builder: (context) => Articles()));
+          //   },
+          //   child: Column(
+          //     children: [
+          //       Icon(Icons.article_rounded, color: page == 'articles' ? Colors.white : Color(0xFFC8C8C8), size: 30),
+          //       SizedBox(height: 3),
+          //       Text('Articles', style: GoogleFonts.poppins(color: page == 'articles' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
+          //     ]
+          //   ),
+          // ),
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.push(context, MaterialPageRoute(builder: (context) => ToDo()));
+          //   },
+          //   child: Column(
+          //     children: [
+          //       Icon(Icons.task_outlined, color: page == 'tasks' ? Colors.white : Color(0xFFC8C8C8), size: 30),
+          //       SizedBox(height: 3),
+          //       Text('Tasks', style: GoogleFonts.poppins(color: page == 'tasks' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
+          //     ]
+          //   )
+          // ),
+          // Column(
+          //   children: [
+          //     Icon(Icons.person_2_rounded, color: page == 'profile' ? Colors.white : Color(0xFFC8C8C8), size: 30),
+          //     SizedBox(height: 3),
+          //     Text('Profile', style: GoogleFonts.poppins(color: page == 'profile' ? Colors.white : Color(0xFFC8C8C8), fontSize: 13, fontWeight: FontWeight.w600))
+          //   ]
+          // )
         ]
       )
     );
