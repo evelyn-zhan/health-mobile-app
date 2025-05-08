1f6c0d8b070e54c10e0c852286cf18a0367d88b8
evelyn-zhan <evelynzanpx@gmail.com>
2025-05-08 22:12:38 +0700

    refactor: centralize Scaffold to main, remove from individual pages

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:health_mobile_app/providers/page_provider.dart';
 import 'package:health_mobile_app/providers/profile_provider.dart';
-import 'package:health_mobile_app/screens/articles.dart';
 import 'package:health_mobile_app/components/featured_article_card.dart';
 import 'package:provider/provider.dart';
 
